2ad151f58672a43165e80d401ad581d0f9b00d4f
AliceInCradle <chenshi@krutt.org>
2024-02-01 16:45:14 +0800

    Sync

--- hello.rs
+++
@@ -1,6 +0,0 @@
-pub(crate) fn main()
-{
-    println!("Hello, welcome to the rust learning!");
-    println!("I hope that you can have a good time learning a new language!");
-    println!("\n");
-}
